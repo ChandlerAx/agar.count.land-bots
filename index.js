914a16eb0c22d32c22764a1bd1cc4630b331e398
ChandlerAx <151396622+ChandlerAx@users.noreply.github.com>
2024-04-29 19:22:24 +0200

    Update index.js
    working mouse directions

--- index.js
+++ index.js
@@ -9,18 +9,26 @@ const bots = [];
 const encoder = new TextEncoder();
 const buffer = new DataView(new ArrayBuffer(1024));
 
+const mouse_controller = {
+    to_bottom: { x: 0, y: 300 },
+    to_up: { x: 0, y: -300 },
+    to_left: { x: -300, y: 0 },
+    to_right: { x: 300, y: 0 },
+};
+
 const packets = {
     spawn: [0, 1],
     chat: [0, 128],
-    mouse: [5, 0]
+    mouse: [0, 7],
 };
 
 async function sock() {
     for (let i = 0; i < botsa; i++) {
-
         let bot;
         try {
-            bot = new WebSocket("wss://count.land:8443/", { rejectUnauthorized: false });
+            bot = new WebSocket("wss://count.land:8443/", {
+                rejectUnauthorized: false,
+            });
         } catch (error) {
             console.error(`${i + 1} `, error);
             continue;
@@ -34,31 +42,29 @@ async function sock() {
             bot.binaryType = "arraybuffer";
 
             function write_payload(arr, data, m) {
-                if(m == false) {
+                if (m == false) {
                     let pce = 1 + encoder.encodeInto(data, new Uint8Array(buffer.buffer, 1)).written;
                     buffer.setUint8(arr[0], arr[1]);
                     bot.send(new Uint8Array(buffer.buffer, 0, pce));
                 } else {
-                    buffer.setUint8(0, 0)
-                    bot.send(new Uint8Array(buffer.buffer, 0, 7));
+                    buffer.setUint8(0, 0);
+                    buffer.setInt16(1, data.x);
+                    buffer.setInt16(3, data.y);
+                    buffer.setInt16(5, 0);
+                    bot.send(new Uint8Array(buffer.buffer, arr[0], arr[1]));
                 }
             }
 
             setTimeout(() => {
                 write_payload(packets.spawn, botname, false);
                 write_payload(packets.chat, botmsg, false);
-                write_payload(0,0,true)
+                write_payload(packets.mouse, { x: mouse_controller.to_left.x, y: mouse_controller.to_left.y }, true);
             }, 1000);
 
-          // prevent timeout
-          setInterval(()=> {
-            write_payload(packets.chat, 'ping', false);
-          }, 5000)
-
-          
+            setInterval(() => {
+                // write_payload(packets.chat, "ping", false);
+            }, 9000);
         });
-
-        bot.on("message", function incoming(data) {});
 
         bot.on("error", function error(err) {
             console.error(`${i + 1} error:`, err);
@@ -69,6 +75,5 @@ async function sock() {
         });
     }
 }
-
 
 sock().catch((err) => console.error("Error spawning bots:", err));
